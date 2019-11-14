2cd47b5fca311cabad6ba5bcda8558a27a8441f1
kak500 <36472752+kak500@users.noreply.github.com>
2019-11-14 06:33:00 +0100

    Add files via upload
    przydzielenie do zbioru c#

---
+++ wprowadzenieDoInf/wprowadzenieDoInf/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wprowadzenieDoInf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string zbior = x < 0 ? "ujemne" : x >= 0 && x <= 9 ? "jednocyfrowe dodatnie" : x >= 10 && x <= 99 ? "dodatnie dwucyfrowe" : "pozostałe";
+            Console.WriteLine(zbior);
+            Console.ReadKey();
+        }
+    }
+}
